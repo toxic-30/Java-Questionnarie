f711671360ad62f10d7bc907a39cea1dedd98de0
toxic-30 <73540247+toxic-30@users.noreply.github.com>
2025-09-04 23:51:21 +0530

    ThreadPoolExecutor framework

---
+++ Core&AdvanceJava/CodeSnippet/mtcCodeSnippet/ExecutorFramework.java
@@ -0,0 +1,10 @@
+/*
+Executor (Interface) is extended by ExecutorService(Interface). Now ExecutorService is extended and implemented
+by below class & interface.
+ThreadPoolExecutor(Class) ->
+ForkJoinPool(Class)
+ScheduledExecutorService(Interface) -> Implements by ScheduleThreadPoolExecutor
+ */
+public class ExecutorFramework{
+
+}
